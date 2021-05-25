d7c92f97afe97f5b64a79175c6780f2b8b8619bb
Ангел Спасов <aspasov_@abv.bg>
2021-05-25 06:52:15 +0300

    Add files via upload

---
+++ 03. Arrays - More Exercises/07.spiral-matrix.js
@@ -0,0 +1,9 @@
+function solve(rows, colomns) {
+    let matrix = [];
+    for (let i = 1; i <= rows*colomns; i++) {
+        matrix[0].push(i);
+        if (i > colomns && i < 2*rows) {
+            
+        }
+    }
+}
---
+++ 03. Arrays - More Exercises/08.diagonal-attack.js
@@ -0,0 +1,8 @@
+function solve(input) {
+    let numbers = [];
+    for (let i = 0; i < input.length; i++) {
+        numbers[i] = input[i].split(" ");
+        numbers[i] = Number;
+    }
+}
+solve(["1 2 3", "4 5 6"]);

a7b97f0736014d965a20bf5e5eeffa3c0e801c3d
Ангел Спасов <aspasov_@abv.bg>
2021-09-24 02:21:54 +0300

    Delete 03.censored-word.js

--- 03.censored-word.js
+++
@@ -1,8 +0,0 @@
-function solve(text, word) {
-    let replacement = "*".repeat(word.length);
-    while (text.includes(word)) {
-        text = text.replace(word, replacement);
-    }
-    console.log(text);
-}
-solve("smallest sentence with small words smalle", "small");

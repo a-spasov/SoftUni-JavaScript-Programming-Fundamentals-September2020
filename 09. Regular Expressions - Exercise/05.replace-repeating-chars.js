b93a8a863756ab339b0553c431f441985208c35f
Ангел Спасов <aspasov_@abv.bg>
2021-10-21 00:14:43 +0300

    Delete 05.replace-repeating-chars.js

--- 09. Regular Expressions - Exercise/05.replace-repeating-chars.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    for (let char of input) {
-        if (input.includes(`${char}${char}`)) {
-            input = input.replace(`${char}${char}`, `${char}`);
-        }
-    }
-    console.log(input);
-}
-solve('aaaaabbbbbcdddeeeedssaa');

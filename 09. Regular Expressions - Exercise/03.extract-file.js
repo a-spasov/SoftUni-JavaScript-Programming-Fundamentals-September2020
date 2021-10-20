e14f0f9ef4edc2cbc570c868f3357dd6c4be6341
Ангел Спасов <aspasov_@abv.bg>
2021-10-21 00:14:21 +0300

    Delete 03.extract-file.js

--- 09. Regular Expressions - Exercise/03.extract-file.js
+++
@@ -1,7 +0,0 @@
-function solve(filePath) {
-    let start = filePath.lastIndexOf("\\");
-    let end = filePath.lastIndexOf(".");
-    console.log(`File name: ${filePath.substring(start + 1, end)}`);
-    console.log(`File extension: ${filePath.substring(end + 1, filePath.length)}`);
-}
-solve('C:\\Internal\\training-internal\\Template.pptx');
